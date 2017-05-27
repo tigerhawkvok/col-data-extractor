271dbe0290c81963f4691ac1c7b6428576fcdeda
Philip Kahn <tigerhawkvok@gmail.com>
2017-05-26 20:18:40 -0700

    change wrapper

--- create_csv.py
+++ create_csv.py
@@ -1,8 +1,8 @@
 """
-CLI wrapper for clean_source_data.py
+CLI wrapper for CleanSourceData.py
 """
 import os, qinput
-import clean_source_data
+import CleanSourceData
 
 validExts = [
     "csv",
@@ -18,9 +18,9 @@ tsv = [
 path = None
 while path is None:
     try:
-        path = qinput.input("Please input the path to the CSV file to be used (default:"+clean_source_data.defaultFile+"): ")
+        path = qinput.input("Please input the path to the CSV file to be used (default:"+CleanSourceData.defaultFile+"): ")
         if path == "":
-            path = clean_source_data.defaultFile
+            path = CleanSourceData.defaultFile
         tmp = path.split(".")
         ext = tmp.pop().lower()
         if len(ext) is not 3 and len(ext) is not 4:
@@ -30,18 +30,18 @@ while path is None:
             # won't exist
             path += ".csv"
         elif not ext in validExts:
-            print("You did not point to a valid CSV file.", clean_source_data.exitScriptPrompt)
+            print("You did not point to a valid CSV file.", CleanSourceData.exitScriptPrompt)
             print("You provided", path)
             path = None
             continue
         # Check the file
         if not os.path.isfile(path):
-            print("Invalid file.", clean_source_data.exitScriptPrompt)
+            print("Invalid file.", CleanSourceData.exitScriptPrompt)
             print("You provided", path)
             path = None
     except KeyboardInterrupt:
-        clean_source_data.doExit()
-if clean_source_data.outputFile is None:
+        CleanSourceData.doExit()
+if CleanSourceData.outputFile is None:
     tmp = path.split(".")
     tmp.pop()
     outputFile = ".".join(tmp) + "-formatted.csv"
@@ -49,5 +49,5 @@ if clean_source_data.outputFile is None:
         delimiter = "\t"
     else:
         delimiter = ","
-    outputPathVerified = clean_source_data.cleanCSV(path, outputFile, delimiter)
+    outputPathVerified = CleanSourceData.cleanCSV(path, outputFile, delimiter)
     print("New file written to '"+outputPathVerified+"'")
